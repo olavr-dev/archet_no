855ae6cd87e4d172bdb258bb8d5837ae376e3375
Olav Rørvik <olavr.dev@gmail.com>
2020-02-23 23:55:15 +0100

    add contact form success page

--- process.php
+++ process.php
@@ -10,4 +10,6 @@
     
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+    
+    header('Location: https://www.archet.no/success.html');
   }
